3dbda6d7692e95dde5db32dc7b4cb859c8952269
luzhengfei <luzhengfei@geekbang.com>
2024-12-29 09:57:58 +0800

    change task def v11

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,3 +13,12 @@ Route::get('/greeting', function () {
     // 写入一条错误日志
     Log::error('This is an error message');
 });
+
+Route::get('/trigger_php_log', function () {
+    // 自定义一条日志信息
+    $message = "This is a custom error log message.";
+    // 使用 error_log 函数写入日志
+    error_log($message);
+    // 写入一条错误日志
+    Log::error('This is an error message');
+});
